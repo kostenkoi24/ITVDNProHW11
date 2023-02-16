f5e4c4718f879a57128d5b99da103057430955a2
kostenkoi24 <kostenko.i@gmail.com>
2023-02-16 16:07:59 +0200

    Homework11

--- Homework11_2/Program.cs
+++ Homework11_2/Program.cs
@@ -1,12 +1,76 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
 
 namespace Homework11_2
 {
     class Program
     {
+        public static string destinationFile = "DestinationFile.txt";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string sourseFile1 = "FirstSourceFile.txt";
+            string sourseFile2 = "SecondSourceFile.txt";
+            
+
+            FileCreator(sourseFile1);
+            FileCreator(sourseFile2);
+
+            ParameterizedThreadStart p_thread1 = new ParameterizedThreadStart(FileCopy);
+            Thread thread1 = new Thread(p_thread1);
+            thread1.Start(sourseFile1);
+            thread1.Join();
+
+            ParameterizedThreadStart p_thread2 = new ParameterizedThreadStart(FileCopy);
+            Thread thread2 = new Thread(p_thread2);
+            thread2.Start(sourseFile2);
+            thread2.Join();
+
+
+            Console.WriteLine($"Data was saved successfully in {destinationFile}");
+
+
+            Console.ReadKey();
+
         }
+
+
+        
+
+
+
+        static void FileCreator(string fileName)
+        {
+            var file = new FileInfo(fileName);
+
+            StreamWriter writer = file.CreateText();
+
+            for (int i = 0; i < 10; i++)
+            {
+                writer.WriteLine(fileName);
+            }
+            writer.Close();
+        }
+
+
+
+        static void FileCopy(object fileName)
+        {
+            Console.WriteLine((string)fileName);
+            
+            var reader = new StreamReader((string)fileName);
+            var writer = new StreamWriter(destinationFile, append: true);
+
+            
+            writer.WriteLine(reader.ReadToEnd());
+            writer.Close();
+
+        }
+
+
+
     }
 }
---
+++ Homework11_4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Homework11_4
+{
+    class Program
+    {
+        static int counter = 0;
+
+        static object block = 0; 
+
+        static void Function()
+        {
+            lock (block)
+            {
+                for (int i = 0; i < 50; ++i)
+                {
+                    Console.WriteLine(++counter);
+                    Console.WriteLine("\t{0}",Thread.CurrentThread.GetHashCode());
+                }
+            }
+            
+        }
+
+        static void Main()
+        {
+            Thread[] threads = { new Thread(Function), new Thread(Function), new Thread(Function) };
+
+            foreach (Thread thread in threads)
+                thread.Start();
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
